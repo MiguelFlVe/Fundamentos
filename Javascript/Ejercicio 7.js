31bd9e566fdf50fe12475f28d5811cc4dccc27e4
Miguel Flórez <miguelfv10@hotmail.com>
2026-02-27 16:31:42 -0500

    Desarrollo del ejercicio 7
    Se desarrolla el ejercicio 7, el solicita un programa que calcule el doble y el triple de un número ingresado por el usuario

---
+++ Javascript/Ejercicio 7.js
@@ -0,0 +1,14 @@
+// Inicio
+
+// Definir variables
+let a, double, triple;
+
+// Solicitar datos
+a = parseFloat(prompt("Ingrese un número:"));
+
+// Calcular doble y triple
+double = a * 2;
+triple = a * 3;
+console.log("El doble de ", a, "es", double, "y su triple es", triple)
+
+// Fin
